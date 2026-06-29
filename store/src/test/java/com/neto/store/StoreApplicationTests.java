59379045a590e4dee3c6c63e2ceafec938f47b51
Anderson Luiz Neto <anderson.neto26@gmail.com>
2026-06-29 18:02:37 -0300

    First commit

---
+++ store/src/test/java/com/neto/store/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neto.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
